9e88c169f800e0e091c7be961255fc367b137e6a
Helo88 <github.com/Helo88>
2020-05-11 02:18:25 +0200

    update cryIf.h

--- src/CryIf/CryIf.h
+++ src/CryIf/CryIf.h
@@ -4,16 +4,20 @@
 #include "Csm_Types.h"
 #include "CryIf_Cfg.h"
 
-/* version macrocs*/
-#define CRYIF_VENDOR_ID (VENDOR_ID_ARCCORE)
-#define CRYIF_MODULE_ID (MODULE_ID_CRYIF)
-#define CRYIF_AR_MAJOR_VERSION 4
-#define CRYIF_AR_MINOR_VERSION 3
-#define CRYIF_AR_PATCH_VERSION 1
+#define MODULE_ID_CRYIF  (uint16)112
 
-#define CRYIF_SW_MAJOR_VERSION 4
-#define CRYIF_SW_MINOR_VERSION 3
-#define CRYIF_SW_PATCH_VERSION 1
+
+
+/* version macrocs*/
+#define CRYIF_VENDOR_ID          (1U)
+#define CRYIF_MODULE_ID          (110U)
+#define CRYIF_AR_MAJOR_VERSION   4
+#define CRYIF_AR_MINOR_VERSION   3
+#define CRYIF_AR_PATCH_VERSION   1
+
+#define CRYIF_SW_MAJOR_VERSION   4
+#define CRYIF_SW_MINOR_VERSION   3
+#define CRYIF_SW_PATCH_VERSION   1
 //-----------------------------------------------------------//
 
 /* Services IDs */
@@ -43,45 +47,37 @@
 #define CRYIF_E_PARAM_HANDLE 0x03
 #define CRYIF_E_PARAM_VALUE 0x04
 #define CRYIF_E_KEY_SIZE_MISMATCH 0x05
-//--------------------------------------------------------------//
-/* extension to Std_ReturnType                   
-#define CRYPTO_E_BUSY 0x02
-#define CRYPTO_E_SMALL_BUFFER 0x03
-#define CRYPTO_E_ENTROPY_EXHAUSTION 0x04
-#define CRYPTO_E_QUEUE_FULL 0x05
-#define CRYPTO_E_KEY_READ_FAIL 0x06
-#define CRYPTO_E_KEY_WRITE_FAIL 0x07
-#define CRYPTO_E_KEY_NOT_AVAILABLE 0x08
-#define CRYPTO_E_KEY_NOT_VALID 0x09
-#define CRYPTO_E_KEY_SIZE_MISMATCH 0x0A
-#define CRYPTO_E_COUNTER_OVERFLOW 0x0B
-#define CRYPTO_E_JOB_CANCELED 0x0C
-*/
-//--------------------------------------------------------------//
 
-//--------------------------------------------------------------//
-void CryIf_Init(void);
-//void CryIf_GetVersionInfo( Std_VersionInfoType* versioninfo );
-Std_ReturnType CryIf_ProcessJob(uint32 channelId, Crypto_JobType *job);
-Std_ReturnType CryIf_CancelJob(uint32 channelId, Crypto_JobType *job);
-Std_ReturnType CryIf_KeyElementSet(uint32 cryIfKeyId, uint32 keyElementId, const uint8 *keyPtr, uint32 keyLength);
-Std_ReturnType CryIf_KeySetValid(uint32 cryIfKeyId);
-Std_ReturnType CryIf_KeyElementGet(uint32 cryIfKeyId, uint32 keyElementId, uint8 *resultPtr, uint32 *resultLengthPtr);
-Std_ReturnType CryIf_KeyElementCopy(uint32 cryIfKeyId, uint32 keyElementId, uint32 targetCryIfKeyId, uint32 targetKeyElementId);
-Std_ReturnType CryIf_KeyCopy(uint32 cryIfKeyId, uint32 targetCryIfKeyId);
-Std_ReturnType CryIf_RandomSeed(uint32 cryIfKeyId, const uint8 *seedPtr, uint32 seedLength);
-Std_ReturnType CryIf_KeyGenerate(uint32 cryIfKeyId);
-Std_ReturnType CryIf_KeyDerive(uint32 cryIfKeyId, uint32 targetCryIfKeyId);
-Std_ReturnType CryIf_KeyExchangeCalcPubVal(uint32 cryIfKeyId, uint8 *publicValuePtr, uint32 *publicValueLengthPtr);
-Std_ReturnType CryIf_KeyExchangeCalcSecret(uint32 cryIfKeyId, const uint8 *partnerPublicValuePtr, uint32 partnerPublicValueLength);
-Std_ReturnType CryIf_CertificateParse(uint32 cryIfKeyId);
-Std_ReturnType CryIf_CertificateVerify(uint32 cryIfKeyId, uint32 verifyCryIfKeyId, Crypto_VerifyResultType *verifyPtr);
 
-void CryIf_CallbackNotification(const Crypto_JobType *job, Std_ReturnType result);
+void CryIf_Init( void );
+void CryIf_GetVersionInfo( Std_VersionInfoType* versioninfo );
+Std_ReturnType CryIf_ProcessJob( uint32 channelId, Crypto_JobType* job );
+Std_ReturnType CryIf_CancelJob( uint32 channelId, Crypto_JobType* job );
+Std_ReturnType CryIf_KeyElementSet( uint32 cryIfKeyId, uint32 keyElementId, const uint8* keyPtr, uint32 keyLength);
+Std_ReturnType CryIf_KeySetValid( uint32 cryIfKeyId);
+Std_ReturnType CryIf_KeyElementGet( uint32 cryIfKeyId, uint32 keyElementId, uint8* resultPtr, uint32* resultLengthPtr);
+Std_ReturnType CryIf_KeyElementCopy( uint32 cryIfKeyId, uint32 keyElementId, uint32 targetCryIfKeyId, uint32 targetKeyElementId );
+Std_ReturnType CryIf_KeyCopy( uint32 cryIfKeyId, uint32 targetCryIfKeyId );
+Std_ReturnType CryIf_RandomSeed( uint32 cryIfKeyId, const uint8* seedPtr, uint32 seedLength);
+Std_ReturnType CryIf_KeyGenerate( uint32 cryIfKeyId);
+Std_ReturnType CryIf_KeyDerive( uint32 cryIfKeyId, uint32 targetCryIfKeyId);
+Std_ReturnType CryIf_KeyExchangeCalcPubVal( uint32 cryIfKeyId, uint8* publicValuePtr, uint32* publicValueLengthPtr);
+Std_ReturnType CryIf_KeyExchangeCalcSecret( uint32 cryIfKeyId, const uint8* partnerPublicValuePtr, uint32 partnerPublicValueLength);
+Std_ReturnType CryIf_CertificateParse( uint32 cryIfKeyId);
+Std_ReturnType CryIf_CertificateVerify( uint32 cryIfKeyId, uint32 verifyCryIfKeyId, Crypto_VerifyResultType* verifyPtr);
 
-/*SRS_BSW_00407 -> SWS_CryIf_91001  */
-#if (CRYIF_VERSION_INFO_API == STD_ON)
-#define CryIf_GetVersionInfo(_vi) (STD_GET_VERSION_INFO((_vi), (CRYIF)))
+void CryIf_CallbackNotification( const Crypto_JobType* job, Std_ReturnType result);
+
+
+
+
+#if ( CRYIF_VERSION_INFO_API == STD_ON )
+#define CryIf_GetVersionInfo(_vi) (STD_GET_VERSION_INFO((_vi),(CRYIF)))
 #endif
 
 #endif
+
+extern CRYIF_KEY KEYS [];
+extern CRYIF_CHANNEL  CHANNELS [];
+
+

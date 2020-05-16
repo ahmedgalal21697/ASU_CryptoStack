e0c42d7eb1eb87e54064107196a3eda0818b0718
Antar <ahmedantar54@gmail.com>
2020-05-16 15:48:51 +0200

    code quality crypto header

--- src/Crypto/Crypto.h
+++ src/Crypto/Crypto.h
@@ -1,20 +1,31 @@
+/*
+ *  -------------------------------------------------------------------------------------------------------------------
+ *  FILE DESCRIPTION
+ *  -----------------------------------------------------------------------------------------------------------------*/
+/*!        \file  Crypto.h
+ *        \brief  MICROSAR Crypto Driver (Crypto)
+ *
+ *      \details  Header file of the MICROSAR Crypto Driver (Crypto)
+ *
+ *********************************************************************************************************************/
+
 #ifndef Crypto_H_
 #define Crypto_H_
+
+/**********************************************************************************************************************
+ *  INCLUDES
+ *********************************************************************************************************************/
+
 #include "Csm_Types.h"
 #include "Crypto_Cfg.h"
-#include "SHA224.h"
-#include "MD5.h"
-#include "SHA1.h"
-#include "SHA256.h"
-#include "SHA384.h"
-#include "SHA512.h"
+
 
 #define CRYPTO_MODULE_ID 114
 #define Objects 1
 
-//Crypto_JobType job_queue[100];
 
-//Services IDs
+
+/*Services IDs*/
 
 #define Crypto_Init_ID 0x00
 #define Crypto_GetVersionInfo_ID 0x01
@@ -23,45 +34,48 @@
 #define Crypto_KeyElementSet_ID 0x04
 #define Crypto_KeySetValid_ID 0x05
 #define Crypto_KeyElementGet_ID 0x06
-//development errors
+/*development errors*/
 #define CRYPTO_E_UNINIT 0x00
 #define CRYPTO_E_INIT_FAILED 0x01
 #define CRYPTO_E_PARAM_POINTER 0x02
 #define CRYPTO_E_PARAM_HANDLE 0x04
 #define CRYPTO_E_PARAM_VALUE 0x05
-//runtime errors
+/*runtime errors*/
 #define CRYPTO_E_RE_SMALL_BUFFER 0x00
 #define CRYPTO_E_RE_KEY_NOT_AVAILABLE 0x01
 #define CRYPTO_E_RE_KEY_READ_FAIL 0x02
 #define CRYPTO_E_RE_ENTROPY_EXHAUSTED 0x03
 
-//General API
+/**********************************************************************************************************************
+ *  FUNCTION PROTOTYPES
+ *********************************************************************************************************************/
+/*General API*/
 
 void Crypto_Init(void);
 void Crypto_GetVersionInfo(Std_VersionInfoType *versioninfo);
-//Job Processing Interface
+/*Job Processing Interface*/
 Std_ReturnType Crypto_ProcessJob(uint32 objectId, Crypto_JobType *job);
-//Job Cancellation Interface
+/*Job Cancellation Interface*/
 Std_ReturnType Crypto_CancelJob(uint32 objectId, Crypto_JobInfoType *job);
-//Key Setting Interface
+/*Key Setting Interface*/
 Std_ReturnType Crypto_KeyElementSet(uint32 cryptoKeyId, uint32 keyElementId, const uint8 *keyPtr, uint32 keyLength);
 Std_ReturnType Crypto_KeySetValid(uint32 cryptoKeyId);
-//Key Extraction Interface
+/*Key Extraction Interface*/
 Std_ReturnType Crypto_KeyElementGet(uint32 cryptoKeyId, uint32 keyElementId, uint8 *resultPtr, uint32 *resultLengthPtr);
 Std_ReturnType Crypto_KeyCopy(uint32 cryptoKeyId, uint32 targetCryptoKeyId);
 Std_ReturnType Crypto_KeyElementIdsGet(uint32 cryptoKeyId, uint32 *keyElementIdsPtr, uint32 *keyElementIdsLengthPtr);
-//Key Generation Interface
+/*Key Generation Interface*/
 Std_ReturnType Crypto_RandomSeed(uint32 cryptoKeyId, const uint8 *seedPtr, uint32 seedLength);
 Std_ReturnType Crypto_KeyGenerate(uint32 cryptoKeyId);
-//Key Derivation Interface
+/*Key Derivation Interface*/
 Std_ReturnType Crypto_KeyDerive(uint32 cryptoKeyId, uint32 targetCryptoKeyId);
-//Key Exchange Interface
+/*Key Exchange Interface*/
 Std_ReturnType Crypto_KeyExchangeCalcPubVal(uint32 cryptoKeyId, uint8 *publicValuePtr, uint32 *publicValueLengthPtr);
 Std_ReturnType Crypto_KeyExchangeCalcSecret(uint32 cryptoKeyId, const uint8 *partnerPublicValuePtr, uint32 partnerPublicValueLength);
-//Certificate Interface
+/*Certificate Interface*/
 Std_ReturnType Crypto_CertificateParse(uint32 cryptoKeyId);
 Std_ReturnType Crypto_CertificateVerify(uint32 cryptoKeyId, uint32 verifyCryptoKeyId, Crypto_VerifyResultType *verifyPtr);
-//Main function
+/*Main function*/
 void Crypto_MainFunction(void);
 
 #endif

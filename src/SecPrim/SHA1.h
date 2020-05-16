768a25af9ef2add54d3b69a7ebe0171b92724a53
Helo88 <github.com/Helo88>
2020-05-16 16:21:14 +0200

    update data types

--- src/SecPrim/SHA1.h
+++ src/SecPrim/SHA1.h
@@ -10,27 +10,27 @@
 
 /*************************** HEADER FILES ***************************/
 #include <stddef.h>
-
+#include "Platform_Types.h"
 /****************************** MACROS ******************************/
-#define SHA1_BLOCK_SIZE 20 // SHA1 outputs a 20 byte digest
+#define SHA1_BLOCK_SIZE 20              // SHA1 outputs a 20 byte digest
 
 /**************************** DATA TYPES ****************************/
-typedef unsigned char BYTE; // 8-bit byte
-typedef unsigned int WORD;	// 32-bit word, change to "long" for 16-bit machines
+//typedef unsigned char uint8;             // 8-bit byte
+//typedef unsigned int  uint32;             // 32-bit word, change to "long" for 16-bit machines
 
-typedef struct
-{
-	BYTE data[64];
-	WORD datalen;
+typedef struct {
+	uint8 data[64];
+	uint32 datalen;
 	unsigned long long bitlen;
-	WORD state[5];
-	WORD k[4];
+	uint32 state[5];
+	uint32 k[4];
 } SHA1_CTX;
 
 /*********************** FUNCTION DECLARATIONS **********************/
 void sha1_init(SHA1_CTX *ctx);
-void sha1_update(SHA1_CTX *ctx, const BYTE data[], size_t len);
-void sha1_final(SHA1_CTX *ctx, BYTE hash[]);
-void sha1(const BYTE data[], size_t len, BYTE hash[], SHA1_CTX *ctx);
+void sha1_update(SHA1_CTX *ctx, const uint8 data[], uint32 len);
+void sha1_final(SHA1_CTX *ctx, uint8 hash[]);
+//void sha1( const uint8 data[],size_t len,uint8** hash,SHA1_CTX *ctx);
+
 
 #endif /* SHA_1_H_ */
